cfebcd1484b5f3ab84f0802e77dc4ef6ace8ecf6
senkuu29 <2223590.deden@smkn-2sbg.sch.id>
2025-03-28 10:11:28 +0700

    pertemuan satu dan dua (pertemuan dua membahas sintack PHP)

---
+++ pertemuan2/index.php
@@ -0,0 +1,60 @@
+<?php
+// pertemuan 2 - PHP Dasar
+// sintac PHP
+
+// Standar PHP
+// echo, print
+// print_r
+// var_dump
+
+// Penulisan sintacks Php
+// 1. PHP di dalam HTML
+// 2.HTML di dalam PHP
+
+// Variable dan tipe data
+// variable
+// tidak boleh diawali dengan angka, tetapi boleh mengandung angka
+// $nama = "deden ahmad jamil";
+// echo "Halo, nama saya $nama";
+
+// operator
+// aritmatika
+// + - * / %
+// $x = 10;
+// $y = 20;
+// echo $x + $y;
+
+// penggabung string / concatenation / concat
+// .
+// $nama_depan = "deden";
+// $nama_tengah = "ahmad";
+// $nama_belakang = "jamil";
+// echo "$nama_depan . $nama_tengah . $nama_belakang";
+
+
+// Assignment
+// =, +=, -=, *=, /=, .=
+// $x = 1;
+// $x += 8;
+// echo $x;
+// $nama = "deden";
+// $nama .= " ";
+// $nama .= "ahmad";
+// $nama .= " ";
+// $nama .= "jamil";
+// echo $nama;
+
+// perbandingan
+// <, >, <=, >=, ==, != (perbandingan ini tidak mengecek tipe data)
+// var_dump (1 == "1");
+
+// identitas
+// ===, !===
+// var_dump (1 === "1")
+
+// logika
+// &&, ||, !
+// $x = 30;
+// var_dump ($x < 20 || $x % 2 == 0);
+?>
+
